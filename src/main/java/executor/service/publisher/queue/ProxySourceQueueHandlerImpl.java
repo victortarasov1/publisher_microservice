6a1a16f34a6d86497a84709dd09f908da5fe7d1b
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-07-15 00:04:20 +0300

    created QueueHandler interface and it`s implementation

---
+++ src/main/java/executor/service/publisher/queue/ProxySourceQueueHandlerImpl.java
@@ -0,0 +1,40 @@
+package executor.service.publisher.queue;
+
+import executor.service.publisher.model.ProxyConfigHolderDto;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ProxySourceQueueHandlerImpl<T> implements QueueHandler<ProxyConfigHolderDto> {
+    private final Queue<ProxyConfigHolderDto> proxies;
+
+    public ProxySourceQueueHandlerImpl() {
+        proxies = new ConcurrentLinkedQueue<>();
+    }
+
+    @Override
+    public void add(ProxyConfigHolderDto element) {
+        proxies.add(element);
+    }
+
+    @Override
+    public void addAll(List<ProxyConfigHolderDto> elements) {
+        proxies.addAll(elements);
+    }
+
+    @Override
+    public Optional<ProxyConfigHolderDto> poll() {
+        return Optional.ofNullable(proxies.poll());
+    }
+
+    @Override
+    public List<ProxyConfigHolderDto> removeAll() {
+        List<ProxyConfigHolderDto> removedProxies = new ArrayList<>();
+        ProxyConfigHolderDto removed = proxies.poll();
+        while (removed != null) {
+            removedProxies.add(removed);
+            removed = proxies.poll();
+        }
+        return removedProxies;
+    }
+}
---
+++ src/main/java/executor/service/publisher/queue/QueueHandler.java
@@ -0,0 +1,14 @@
+package executor.service.publisher.queue;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QueueHandler<T> {
+    void add(T element);
+
+    void addAll(List<T> elements);
+
+    Optional<T> poll();
+
+    List<T> removeAll();
+}
