4e830b9a6f58667a3f754660aafeab6afd4f00cd
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-07-18 15:02:33 +0300

    wrote filters

---
+++ src/main/java/executor/service/publisher/config/filter/AuthorizationFilter.java
@@ -0,0 +1,31 @@
+package executor.service.publisher.config.filter;
+
+import executor.service.publisher.authorization.TokenBasedAuthorization;
+import executor.service.publisher.config.filter.enums.AuthorizationType;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+public class AuthorizationFilter extends OncePerRequestFilter {
+    private final TokenBasedAuthorization tokenBasedAuthorization;
+
+    public AuthorizationFilter(TokenBasedAuthorization tokenBasedAuthorization) {
+        this.tokenBasedAuthorization = tokenBasedAuthorization;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader(AUTHORIZATION);
+        if (header != null && header.startsWith(AuthorizationType.BEARER.getPrefix()))
+            tokenBasedAuthorization.authorizeIfTokenValid(header.substring(AuthorizationType.BEARER.getPrefix().length()));
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/executor/service/publisher/config/filter/ExceptionHandlingFilter.java
@@ -0,0 +1,35 @@
+package executor.service.publisher.config.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import executor.service.publisher.exception.security.SecurityAuthException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@Component
+public class ExceptionHandlingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (SecurityAuthException ex) {
+            record SecurityExceptionResponse(String message, List<String> debugMessage) {
+            }
+            response.setHeader(HttpHeaders.WWW_AUTHENTICATE, ex.getMessage());
+            response.setStatus(FORBIDDEN.value());
+            SecurityExceptionResponse error = new SecurityExceptionResponse(ex.getMessage(), List.of(ex.getCause().getMessage()));
+            response.setContentType(APPLICATION_JSON_VALUE);
+            new ObjectMapper().writeValue(response.getOutputStream(), error);
+        }
+    }
+}
