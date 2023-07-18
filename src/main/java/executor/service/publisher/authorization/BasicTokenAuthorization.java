11739f84f0fe1fc8d4950f5c94c1810bdf051456
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-07-18 18:16:05 +0300

    Refactored for achieving loose coupling

--- src/main/java/executor/service/publisher/authorization/BasicTokenAuthorization.java
+++ src/main/java/executor/service/publisher/authorization/BasicTokenAuthorization.java
@@ -1,35 +1,30 @@
 package executor.service.publisher.authorization;
 
-import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
-import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import executor.service.publisher.exception.security.AuthorizationException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 
-@Component
 public class BasicTokenAuthorization implements TokenBasedAuthorization {
-    private final Algorithm algorithm;
 
-    public BasicTokenAuthorization(@Value("${jwt.secret.key}") String SECRET_KEY) {
-        this.algorithm = Algorithm.HMAC256(SECRET_KEY.getBytes());
+    private final JWTVerifier verifier;
+
+    public BasicTokenAuthorization(JWTVerifier verifier) {
+        this.verifier = verifier;
     }
+
 
     @Override
     public void authorizeIfTokenValid(String jwtToken) {
         try {
-            JWTVerifier verifier = JWT.require(algorithm).build();
             DecodedJWT decodedJWT = verifier.verify(jwtToken);
             String username = decodedJWT.getSubject();
             UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, null);
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        } catch (TokenExpiredException | JWTDecodeException | SignatureVerificationException ex) {
+        } catch (JWTDecodeException | SignatureVerificationException ex) {
             throw new AuthorizationException(ex);
         }
     }
