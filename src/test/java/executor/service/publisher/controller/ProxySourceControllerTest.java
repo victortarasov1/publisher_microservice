847e7558b34d52f3265baa939b7b265f25a1cee5
OleksandrShevchenko21 <shevchenko2106@gmail.com>
2023-07-18 17:13:58 +0300

    updated according to the comments

--- src/test/java/executor/service/publisher/controller/ProxySourceControllerTest.java
+++ src/test/java/executor/service/publisher/controller/ProxySourceControllerTest.java
@@ -2,7 +2,6 @@ package executor.service.publisher.controller;
 
 import executor.service.publisher.model.ProxyConfigHolderDto;
 import executor.service.publisher.queue.QueueHandler;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
