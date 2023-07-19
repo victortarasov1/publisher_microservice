c8262c8b2961b54dd1986c70a15e1471829cf698
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-07-19 03:00:11 +0300

    simplified configuration

--- src/main/java/executor/service/publisher/config/SecurityConfig.java
+++ src/main/java/executor/service/publisher/config/SecurityConfig.java
@@ -23,33 +23,18 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    private final ExceptionHandlingFilter exceptionHandlingFilter;
+
     @Value("${jwt.secret.key}")
     private String SECRET_KEY;
 
-    public SecurityConfig(ExceptionHandlingFilter exceptionHandlingFilter) {
-        this.exceptionHandlingFilter = exceptionHandlingFilter;
-    }
     @Bean
     public JWTVerifier verifier() {
         Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY.getBytes());
         return JWT.require(algorithm).build();
     }
-
-    @Bean
-    public TokenBasedAuthorization tokenBasedAuthorization(JWTVerifier verifier) {
-        return new BasicTokenAuthorization(verifier);
-    }
-
-    @Bean
-    public AuthorizationFilter authorizationFilter(TokenBasedAuthorization tokenBasedAuthorization) {
-        return new AuthorizationFilter(tokenBasedAuthorization);
-    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthorizationFilter authorizationFilter) throws Exception {
         http
-                .addFilterBefore(authorizationFilter, BasicAuthenticationFilter.class)
-                .addFilterBefore(exceptionHandlingFilter, AuthorizationFilter.class)
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
