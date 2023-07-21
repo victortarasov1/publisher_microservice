1abbc81af0480d55e34ae1c2b29b2a3643de8b94
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-07-21 14:56:22 +0300

    Implemented connection to proxy provider API Created validator class for API endpoint

---
+++ src/main/java/executor/service/publisher/maintenance/validator/ProxyEndpointValidator.java
@@ -0,0 +1,26 @@
+package executor.service.publisher.maintenance.validator;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class ProxyEndpointValidator {
+
+    @Value("${api.validator.base-url}")
+    private String baseUrl;
+
+    public boolean validate(String apiEndpoint) {
+        if(!apiEndpoint.contains(this.baseUrl)) return false;
+
+        String endpointPattern = "(&\\w+=.+){0,10}(&code=\\d{15})";
+
+        Pattern pattern = Pattern.compile(endpointPattern);
+        Matcher matcher = pattern.matcher(apiEndpoint);
+
+        return matcher.find();
+    }
+
+}
