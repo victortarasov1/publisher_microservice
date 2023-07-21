4272d21be521749990faf051ecb00860bb64d347
Oleksii Drabchak <drabchak.aleksey@gmail.com>
2023-07-21 14:51:45 +0300

    Changed Config class:   - Created bean of OkHttpClient   - Added proxy-api-connection config file to property source

--- src/main/java/executor/service/publisher/config/Config.java
+++ src/main/java/executor/service/publisher/config/Config.java
@@ -7,12 +7,15 @@ import executor.service.publisher.model.ScenarioDto;
 import executor.service.publisher.queue.ProxySourceQueueHandler;
 import executor.service.publisher.queue.QueueHandler;
 import executor.service.publisher.queue.ScenarioSourceQueueHandler;
+import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Configuration
+@PropertySource("classpath:proxy-api-connection.properties")
 public class Config {
     @Bean
     public QueueHandler<ProxyConfigHolderDto> proxyQueueHandler() {
@@ -27,5 +30,10 @@ public class Config {
     @Bean
     public ScenarioSourceController scenarioSourceController(QueueHandler<ScenarioDto> handler) {
         return new ScenarioSourceControllerImpl(handler);
+    }
+
+    @Bean
+    public OkHttpClient getOkHttpClient() {
+        return new OkHttpClient();
     }
 }
