5b2246aa41cb4c8c5ff8c402e8984dbf73864425
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-08-11 15:10:47 +0300

    updated main class

--- src/main/java/executor/service/publisher/PublisherApplication.java
+++ src/main/java/executor/service/publisher/PublisherApplication.java
@@ -1,34 +1,13 @@
 package executor.service.publisher;
 
-
-import executor.service.publisher.maintenance.ProxySourceServiceUrl;
-import executor.service.publisher.maintenance.ProxySourceServiceFile;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
 @SpringBootApplication
-public class PublisherApplication implements CommandLineRunner {
-
-	private final ProxySourceServiceFile proxySourceServiceFile;
-	private final ProxySourceServiceUrl proxySourceServiceUrl;
-
-	public PublisherApplication(ProxySourceServiceFile proxySourceServiceFile,
-								ProxySourceServiceUrl proxySourceServiceUrl) {
-		this.proxySourceServiceFile = proxySourceServiceFile;
-		this.proxySourceServiceUrl = proxySourceServiceUrl;
-	}
-
+public class PublisherApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PublisherApplication.class, args);
-	}
-
-
-	@Override
-	public void run(String... args) throws Exception {
-		proxySourceServiceFile.getProxy();
-		proxySourceServiceUrl.requestProxies();
 	}
 }
