3f24b826f5d336ca8cb4d8cc475fd6cc38087666
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-08-11 16:35:49 +0300

    wrote tests

---
+++ src/test/java/executor/service/publisher/source/ProxySourceServiceFileTest.java
@@ -0,0 +1,29 @@
+package executor.service.publisher.source;
+
+import executor.service.publisher.model.ProxyConfigHolderDto;
+import executor.service.publisher.queue.QueueHandler;
+import executor.service.publisher.source.reader.FileReader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.mockito.Mockito.*;
+
+class ProxySourceServiceFileTest {
+
+    private FileReader reader;
+    private QueueHandler<ProxyConfigHolderDto> handler;
+    private SourceService service;
+    @BeforeEach
+    void setUp() {
+        reader = mock(FileReader.class);
+        handler = mock(QueueHandler.class);
+        service = new ProxySourceServiceFile(reader, handler);
+    }
+    @Test
+    void testLoadData() {
+        service.loadData();
+        verify(handler, times(1)).addAll(anyList());
+        verify(reader, times(1)).readData(any(), eq(ProxyConfigHolderDto.class));
+    }
+}
