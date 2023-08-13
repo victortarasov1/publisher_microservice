b831b3fd7cf47718169779be35ba91f219e58704
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-08-13 03:42:35 +0300

    wrote RemoteController and it`s implementation

---
+++ src/main/java/executor/service/publisher/controller/ProxyRemoteSourceController.java
@@ -0,0 +1,26 @@
+package executor.service.publisher.controller;
+
+import executor.service.publisher.model.ProxySourceDto;
+import executor.service.publisher.processing.RemoteProcessingService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Component
+@RequestMapping("/publisher/proxy/remote")
+public class ProxyRemoteSourceController implements RemoteSourceController<ProxySourceDto> {
+    private final RemoteProcessingService<ProxySourceDto> service;
+
+    public ProxyRemoteSourceController(RemoteProcessingService<ProxySourceDto> service) {
+        this.service = service;
+    }
+
+    @Override
+    public void loadFromDefaultRemoteSource() {
+        service.loadFromDefaultRemoteSource();
+    }
+
+    @Override
+    public void loadFromCustomRemoteSource(ProxySourceDto dto) {
+        service.loadFromCustomRemoteSource(dto);
+    }
+}
