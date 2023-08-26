f81f77dd413720ff39e8b6c94e15485d104ad24f
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-08-26 22:38:43 +0300

    updated tests

--- src/test/java/executor/service/publisher/processing/proxy/ProxyProcessingServiceImplTest.java
+++ src/test/java/executor/service/publisher/processing/proxy/ProxyProcessingServiceImplTest.java
@@ -1,5 +1,6 @@
 package executor.service.publisher.processing.proxy;
 
+import executor.service.publisher.exception.validator.UnknownProxyTypeException;
 import executor.service.publisher.model.ProxyConfigHolder;
 import executor.service.publisher.model.ProxySource;
 import executor.service.publisher.queue.proxy.ProxySourceQueueHandler;
@@ -11,34 +12,47 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
 class ProxyProcessingServiceImplTest {
+    private static final String PROXY_TYPE = "http";
+    private static final String BAD_PROXY_TYPE = "";
     private ProxySourceQueueHandler queueHandler;
     private ProxyValidator validator;
     private ProxyProcessingService service;
+    private ProxySource defaultSource;
 
     @BeforeEach
     void setUp() {
         queueHandler = mock(ProxySourceQueueHandler.class);
         validator = mock(ProxyValidator.class);
-        ProxySource defaultDto = new ProxySource("https://some/url", "url", "http");
-        when(validator.getType()).thenReturn(defaultDto.getType());
-        service = new ProxyProcessingServiceImpl(List.of(validator), queueHandler, defaultDto);
+        defaultSource = mock(ProxySource.class);
+        when(validator.getType()).thenReturn(PROXY_TYPE);
+        service = new ProxyProcessingServiceImpl(List.of(validator), queueHandler, defaultSource);
     }
 
     @Test
     void testAdd() {
         ProxyConfigHolder proxy = new ProxyConfigHolder();
+        when(defaultSource.getType()).thenReturn(PROXY_TYPE);
         when(validator.isValid(any(ProxyConfigHolder.class))).thenReturn(true);
         service.add(proxy);
         verify(queueHandler, timeout(2000).times(1)).add(any(ProxyConfigHolder.class));
     }
 
     @Test
+    void testAdd_shouldThrowUnknownProxyTypeException() {
+        ProxyConfigHolder proxy = new ProxyConfigHolder();
+        when(defaultSource.getType()).thenReturn(BAD_PROXY_TYPE);
+        assertThatThrownBy(() -> service.add(proxy)).isInstanceOf(UnknownProxyTypeException.class);
+    }
+
+    @Test
     void testAddAll() {
         List<ProxyConfigHolder> proxies = List.of(new ProxyConfigHolder(), new ProxyConfigHolder());
+        when(defaultSource.getType()).thenReturn(PROXY_TYPE);
         when(validator.isValid(any(ProxyConfigHolder.class))).thenReturn(true);
         service.addAll(proxies);
         verify(queueHandler, timeout(2000).times(2)).add(any(ProxyConfigHolder.class));
