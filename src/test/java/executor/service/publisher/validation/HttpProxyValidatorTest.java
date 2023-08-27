3afecb593c4dfbcb139eb630d12fd2f8c2312e22
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-08-28 01:24:25 +0300

    refactored getRequest method

--- src/test/java/executor/service/publisher/validation/HttpProxyValidatorTest.java
+++ src/test/java/executor/service/publisher/validation/HttpProxyValidatorTest.java
@@ -1,7 +1,6 @@
 package executor.service.publisher.validation;
 
 import executor.service.publisher.model.ProxyConfigHolder;
-import executor.service.publisher.model.ProxyCredentials;
 import executor.service.publisher.model.ProxyNetworkConfig;
 import okhttp3.OkHttpClient;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,14 +21,14 @@ class HttpProxyValidatorTest {
 
     @Test
     void testIsValid_shouldReturnTrue() {
-        ProxyConfigHolder dto = new ProxyConfigHolder(new ProxyNetworkConfig("20.210.113.32", 8080), new ProxyCredentials());
+        ProxyConfigHolder dto = new ProxyConfigHolder(new ProxyNetworkConfig("20.210.113.32", 8080));
         boolean valid = validator.isValid(dto);
         assertThat(valid).isTrue();
     }
 
     @Test
     void testIsValid_shouldReturnFalse() {
-        ProxyConfigHolder dto = new ProxyConfigHolder(new ProxyNetworkConfig("20.201.114.32", 3030), new ProxyCredentials());
+        ProxyConfigHolder dto = new ProxyConfigHolder(new ProxyNetworkConfig("20.201.114.32", 3030));
         boolean valid = validator.isValid(dto);
         assertThat(valid).isFalse();
     }
