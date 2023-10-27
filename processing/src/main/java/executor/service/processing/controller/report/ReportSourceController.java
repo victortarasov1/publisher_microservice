632cee6ab57909dd378bb89c18b99f5c8da38eaa
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-10-27 14:38:32 +0300

    added request mapping

--- processing/src/main/java/executor/service/processing/controller/report/ReportSourceController.java
+++ processing/src/main/java/executor/service/processing/controller/report/ReportSourceController.java
@@ -5,9 +5,10 @@ import executor.service.processing.controller.SourceController;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-
+@RequestMapping("/publisher/proxy")
 public interface ReportSourceController extends SourceController<ScenarioReport> {
 
     @PreAuthorize("isAuthenticated()")
