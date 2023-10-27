bcbddaf05a486781b6eb814134f68277c3ca233c
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-10-27 14:01:35 +0300

    created collection for reports

---
+++ collection/src/main/java/executor/service/collection/queue/report/ReportQueueHandlerImpl.java
@@ -0,0 +1,37 @@
+package executor.service.collection.queue.report;
+
+import executor.service.collection.queue.QueueHandler;
+import executor.service.model.ScenarioReport;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class ReportQueueHandlerImpl implements ReportQueueHandler {
+    private final QueueHandler<ScenarioReport> handler;
+    @Override
+    public void add(ScenarioReport element) {
+        handler.add(element);
+    }
+
+    @Override
+    public void addAll(List<ScenarioReport> elements) {
+        handler.addAll(elements);
+    }
+
+    @Override
+    public Optional<ScenarioReport> poll() {
+        return handler.poll();
+    }
+
+    @Override
+    public List<ScenarioReport> removeAll() {
+        return handler.removeAll();
+    }
+
+    @Override
+    public List<ScenarioReport> removeByCount(int size) {
+        return handler.removeByCount(size);
+    }
+}
