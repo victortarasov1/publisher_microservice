7d2a70520c914f7a0ea340968b7c8404f9c52bc7
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-10-27 13:06:44 +0300

    refactored

--- model/src/main/java/executor/service/model/Scenario.java
+++ model/src/main/java/executor/service/model/Scenario.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,7 +15,7 @@ public class Scenario {
     private UUID uuid;
     private String name;
     private String site;
-    private List<Step> steps;
+    private List<Step> steps = new ArrayList<>();
 
     public void setUUID(UUID uuid) {
         this.uuid = uuid;
