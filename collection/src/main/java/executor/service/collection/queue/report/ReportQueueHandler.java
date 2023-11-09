9944cf0b9101c60e380d6f11e608d68212401435
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-10 00:35:59 +0200

    removed collection module

--- collection/src/main/java/executor/service/collection/queue/report/ReportQueueHandler.java
+++
@@ -1,7 +0,0 @@
-package executor.service.collection.queue.report;
-
-import executor.service.collection.queue.QueueHandler;
-import executor.service.model.ScenarioReport;
-
-public interface ReportQueueHandler extends QueueHandler<ScenarioReport> {
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 rootProject.name = "publisher_microservice"
 include("logger")
 include("model")
-include("collection")
 include("source")
 include("security")
 include("validator")
