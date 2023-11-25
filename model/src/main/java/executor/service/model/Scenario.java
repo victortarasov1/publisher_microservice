996b1cd9bba0cb2ce0e7e8379bd720964a846367
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-25 23:28:47 +0200

    added id field

--- model/src/main/java/executor/service/model/Scenario.java
+++ model/src/main/java/executor/service/model/Scenario.java
@@ -3,5 +3,5 @@ package executor.service.model;
 
 import java.util.List;
 
-public record Scenario(String name, String site, List<Step> steps) {
+public record Scenario(String id, String name, String site, List<Step> steps) {
 }
