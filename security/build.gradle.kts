091c61b045efa564954e1007fb793fe247e90d72
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-01-12 19:49:57 +0200

    refactored

--- security/build.gradle.kts
+++ security/build.gradle.kts
@@ -2,4 +2,6 @@ dependencies {
     implementation("com.auth0:java-jwt:4.4.0")
     implementation(project(":model"))
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
 }
